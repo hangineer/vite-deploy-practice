005a4f0635ad1dd5a16ab91f44446e14e09ce564
hannah <hanforwork896@gmail.com>
2023-01-22 19:58:00 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+// axios
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+import App from "./App.vue";
+import router from "./router";
+
+
+import "./assets/main.css";
+
+
+
+const app = createApp(App);
+app.use(createPinia());
+app.use(router);
+app.use(VueAxios, axios);
+
+app.mount("#app");
